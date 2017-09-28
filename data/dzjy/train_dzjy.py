941276358b319540fac71375f40f6c977d72e3c1
lentopaz <len.topaz@gmail.com>
2017-09-28 18:35:31 +0800

    add daily train task

--- data/dzjy/train_dzjy.py
+++ data/dzjy/train_dzjy.py
@@ -4,8 +4,6 @@ import tensorflow as tf
 import config
 import MySQLdb as mdb
 import os
-import datetime
-import decimal
 
 #市场
 markets = ['sh','sz']
@@ -14,62 +12,72 @@ markets = ['sh','sz']
 rnn_unit=30
 input_size=7
 output_size=1
+
+
 #学习率
 lr=0.0006
 
 conn = None
 cursor = None
 
-TRAINING_STEPS = 2000
+TRAINING_STEPS = 200
+
+
 
 #获取训练集
-def get_train_data(code,batch_size,time_step,term,begin,end):
+def get_train_data(code,batch_size,time_step,term,date):
     stock_history_list = []
     stock_price_list = []
     cursor.execute(''.join([
-            '        SELECT ',
-            '            t.trade_num, ',
-            '            t.fq_close_price, ',
-            '            t.trade_money,  ',
-            '            t.circulation_value,  ',
-            '            t.trade_money / t.circulation_value * 100 trade_rate, ',
-            '            t.turnover_rate, ',
-            '            ifnull( ',
-            '                sum(t2.tvol * t2.PRICE * 10000) / t.circulation_value * 100, ',
-            '                0 ',
-            '            ) dzjy_rate, ',
-            '            d.future_price_30, ',
-            '            t.date ',
+            '       SELECT ',
+            '            * ',
             '        FROM ',
-            '            stock_history t ',
-            '        LEFT JOIN dzjy_history t2 ON t.date = t2.tdate ',
-            '        AND t.`code` = t2.secucode ',
-            '        INNER JOIN stock_train_data d ON d.`code` = t.`code` ',
-            '        AND d.date = t.date ',
-            '        WHERE ',
-            '            t.date <= %s  ',
-            '        AND t.date >= %s  ',
-            '        AND t.`code` = %s  ',
-            '        AND t.date IS NOT NULL ',
-            '        AND t.trade_num IS NOT NULL ',
-            '        AND t.trade_money IS NOT NULL ',
-            '        AND t.fq_close_price IS NOT NULL ',
-            '        AND t.turnover_rate IS NOT NULL ',
-            '        AND d.future_price_30 IS NOT NULL ',
-            '        AND t.close_price IS NOT NULL ',
-            '        AND t.circulation_value IS NOT NULL ',
-            '        GROUP BY ',
-            '            t.date, ',
-            '            t.close_price, ',
-            '            t.trade_num, ',
-            '            t.trade_money, ',
-            '            t.fq_close_price, ',
-            '            t.circulation_value, ',
-            '            t.turnover_rate, ',
-            '            d.future_price_30 ',
+            '            ( ',
+            '                SELECT ',
+            '                    t.trade_num, ',
+            '                    t.fq_close_price, ',
+            '                    t.trade_money,  ',
+            '                    t.circulation_value,  ',
+            '                    t.trade_money / t.circulation_value * 100 trade_rate, ',
+            '                    t.turnover_rate, ',
+            '                    ifnull( ',
+            '                        sum(t2.tvol * t2.PRICE * 10000) / t.circulation_value * 100, ',
+            '                        0 ',
+            '                    ) dzjy_rate, ',
+            '                    d.future_price_30, ',
+            '                    t.date ',
+            '                FROM ',
+            '                    stock_history t ',
+            '                LEFT JOIN dzjy_history t2 ON t.date = t2.tdate ',
+            '                AND t.`code` = t2.secucode ',
+            '                LEFT JOIN stock_train_data d ON d.`code` = t.`code` ',
+            '                AND d.date = t.date ',
+            '                WHERE ',
+            '                    t.date <= %s  ',
+            '                AND t.`code` = %s  ',
+            '                AND t.date IS NOT NULL ',
+            '                AND t.trade_num IS NOT NULL ',
+            '                AND t.trade_money IS NOT NULL ',
+            '                AND t.fq_close_price IS NOT NULL ',
+            '                AND t.turnover_rate IS NOT NULL ',
+            '                AND d.future_price_30 IS NOT NULL ',
+            '                AND t.close_price IS NOT NULL ',
+            '                GROUP BY ',
+            '                    t.date, ',
+            '                    t.close_price, ',
+            '                    t.trade_num, ',
+            '                    t.trade_money, ',
+            '                    t.fq_close_price, ',
+            '                    t.turnover_rate, ',
+            '                    d.future_price_30 ',
+            '                ORDER BY ',
+            '                    t.date DESC ',
+            '                LIMIT 0, ',
+            '                1500 ',
+            '            ) tt ',
             '        ORDER BY ',
-            '            t.date ASC '
-        ]) , [end,begin,code])
+            '            date ASC',
+        ]) , [date,code])
     results = cursor.fetchall()
     for result in results:
         stock_history = []
@@ -103,28 +111,23 @@ def get_train_data(code,batch_size,time_step,term,begin,end):
             continue
         stock_history.append(result[6])
 
-
-
         if result[7] is None:
             continue
         stock_price.append(result[7])
         
         stock_history_list.append(stock_history)
         stock_price_list.append(stock_price)
-
     conn.commit()
 
     stock_history_arr = np.array(stock_history_list)
     stock_price_arr = np.array(stock_price_list)
 
     #全局标准化
-
     if len(stock_history_arr) == 0:
         return [],[],[]
 
     trade_num_arr = stock_history_arr[:,0]
     trade_num_arr = (trade_num_arr - np.mean(trade_num_arr,axis=0)) / np.std(trade_num_arr,axis=0)
-
 
     fq_close_price_arr = stock_history_arr[:,1]
     mean = np.mean(fq_close_price_arr,axis=0)
@@ -143,9 +146,6 @@ def get_train_data(code,batch_size,time_step,term,begin,end):
     stock_history_arr[:,3] = circulation_value
     stock_price_arr = (stock_price_arr - np.mean(stock_price_arr,axis=0)) / np.std(stock_price_arr,axis=0)
 
-
-
-    
     #训练集输入
     train_x = []
     #训练集输出
@@ -157,17 +157,13 @@ def get_train_data(code,batch_size,time_step,term,begin,end):
            batch_index.append(i)
         x = stock_history_arr[i:i+time_step,:8]
         y = stock_price_arr[i:i+time_step,0,np.newaxis]
-
-
-
-
         train_x.append(x.tolist())
         train_y.append(y.tolist())
 
     batch_index.append((len(stock_history_arr)-time_step))
     return batch_index,train_x,train_y
 
-def train_lstm(code,batch_size,time_step,term,begin,end):
+def daily_train_lstm(code,batch_size,time_step,term,date):
     with tf.variable_scope(code + '_' + term, reuse=None):
         code_path = '/home/ayesha/data/models/'+code
         model_path = code_path + '/' + term
@@ -179,7 +175,7 @@ def train_lstm(code,batch_size,time_step,term,begin,end):
 
         X=tf.placeholder(tf.float32, shape=[None,time_step,input_size])
         Y=tf.placeholder(tf.float32, shape=[None,time_step,output_size])
-        batch_index,train_x,train_y = get_train_data(code,batch_size,time_step,term,begin,end)
+        batch_index,train_x,train_y = get_train_data(code,batch_size,time_step,term,date)
         weights={
              'in':tf.Variable(tf.random_normal([input_size,rnn_unit])),
              'out':tf.Variable(tf.random_normal([rnn_unit,1]))
@@ -201,36 +197,33 @@ def train_lstm(code,batch_size,time_step,term,begin,end):
         output=tf.reshape(output_rnn,[-1,rnn_unit]) #作为输出层的输入
         w_out=weights['out']
         b_out=biases['out']
-        pred=tf.matmul(output,w_out)+b_out
-
-        #损失函数
-        #loss=abs(pred[-1][-1]-Y[-1][-1])
-
+        pred = tf.matmul(output,w_out) + b_out
 
         loss=tf.reduce_mean(tf.square(tf.reshape(pred[time_step-1::time_step],[-1])-tf.reshape(Y[:,-1], [-1])))
-
-        #learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, 2001 * (len(batch_index)-1), len(batch_index)-1, LEARNING_RATE_DECAY)
 
         global_step = tf.Variable(0,name='global_step',trainable=False)
         train_op=tf.train.AdamOptimizer(lr).minimize(loss,global_step=global_step)
         saver=tf.train.Saver(tf.global_variables(),max_to_keep=0)
 
         with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
+        	#参数恢复
+            code_path = '/home/ayesha/data/models/'+code
+            model_path = code_path + '/' + term
+            module_file = tf.train.latest_checkpoint(model_path)
+            saver.restore(sess, module_file)
+            #训练
             for i in range(TRAINING_STEPS + 1):
                 for step in range(len(batch_index)-1):
                     final_states,loss_=sess.run([train_op,loss],feed_dict={X:train_x[batch_index[step]:batch_index[step+1]],Y:train_y[batch_index[step]:batch_index[step+1]]})
-                    #print(train_y[batch_index[step]:batch_index[step+1]][-1][-1])
-                    #print pred.eval()
-                if i % 10==0:
-                    print("save model : ", saver.save(sess, model_path + '/stock.model',global_step=global_step))
+            #保存模型
+            print("save model : ", saver.save(sess, model_path + '/stock.model',global_step=global_step))
 
 #训练函数
-def train(batch_size,time_step,term,begin,end):
+def daily_train(batch_size,time_step,term,date):
     for market in markets:
         code_list = getCodeList(market)
         for code in code_list:
-            train_lstm(code,batch_size,time_step,term,begin,end)
+            daily_train_lstm(code,batch_size,time_step,term,begin,end)
 
 def getCodeList(market):
     stock_list = []
@@ -263,16 +256,11 @@ def db_connect():
     cursor = conn.cursor()
 
 def db_close():
-    #关闭数据库连接
     cursor.close()
     conn.close()
 
 if __name__ == '__main__':
     db_connect()
-    #train_lstm(code,batch_size,time_step,term,begin,end):
-    #train_lstm('600000',80 , 30 , '30' , '2005-01-01' , '2012-12-31')
-    train( 80 , 30 , '30' , '2005-01-01' , '2016-12-31' )
-    #train( 90 , 90 , '90' , '2005-01-01' , '2005-01-01' )
-    #train( 180 , 180 , '180' , '2005-01-01' , '2005-01-01' )
-    #train( 360 , 360 , '360' , '2005-01-01' , '2005-01-01' )
+    #daily_train(80,30,'30','2013-01-01')
+    daily_train_lstm('600000',80,30,'30','2013-01-01')
     db_close()
