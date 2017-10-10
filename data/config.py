d595fd55dcb80646a666e99bc1f9762d64d193e9
lentopaz <len.topaz@gmail.com>
2017-10-10 10:55:04 +0800

    modify project structure

--- data/config.py
+++ data/config.py
@@ -1,13 +1,12 @@
 #coding=utf-8
 
-
-#数据库配置
-mysql_ip = '106.15.72.40'
-mysql_port = 3306
-mysql_user = 'ayesha'
-mysql_pass = 'Ayesha_topaz77'
-mysql_db = 'ayesha'
-
-appcode = 'f86b2760312b41a0b9ef963ceca71b0b'
+markets = ['sh']
 
 lstm_data_size = 2000
+
+
+MAX_DATA_SIZE = 1500
+
+TRAINING_STEPS = 200
+
+DAILY_TRAINING_STEPS = 20
