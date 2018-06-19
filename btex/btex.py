3460d34440d527ef3fbeacdc3ab4c6925c6c974b
ishuria <len.topaz@gmail.com>
2018-06-19 18:30:46 +0800

    shuashuashua

--- btex/btex.py
+++ btex/btex.py
@@ -36,11 +36,14 @@ def order_book():
     response = session.post(url,[],verify=True)
     # print(response.text)
     response_data = json.loads(response.text)
-
     buy_data = response_data['data']['buy']
     sell_data = response_data['data']['sell']
-    print(buy_data[0])
-    print(sell_data[0])
+
+    buy_price = buy_data[0]['price']
+    sell_price = sell_data[0]['price']
+
+
+    return buy_price,sell_price
 
 def trade_history():
     ssl._create_default_https_context = ssl._create_unverified_context
@@ -60,7 +63,7 @@ def login():
     print(response.text)
 
 def trade():
-    response = session.post('https://btex.com/trade/BT_ETH',[],verify=True)
+    response = session.post('https://btex.com/trade/'+trade_pair,[],verify=True)
     p = re.compile(r'<input type=\"hidden\" id=\"csrf\" value=\"(.+?)\" />')
     m = p.search(response.text)
     print(m.group())
@@ -68,12 +71,52 @@ def trade():
 
     url = "https://btex.com/priapi1/buy_coin"
     values = {}
-    values['price'] = 0.001
-    values['num'] = 1.1523
-    values['type'] = 'BT'
+    values['price'] = 0.00000900
+    values['num'] = 2000
+    values['type'] = 'TCO'
     values['danwei'] = 'ETH'
     values['csrf'] = csrf
-    values['trade_psw'] = 'btex201806191'
+    values['trade_psw'] = 'btex20180619'
+    
+    #临时解决https的问题
+    response = session.post(url,values,verify=True)
+    print(response.text)
+
+def trade_eth_tco():
+    response = session.post('https://btex.com/trade/'+trade_pair,[],verify=True)
+    p = re.compile(r'<input type=\"hidden\" id=\"csrf\" value=\"(.+?)\" />')
+    m = p.search(response.text)
+    print(m.group())
+    csrf = m.group()[38:70]
+
+    url = "https://btex.com/priapi1/buy_coin"
+    values = {}
+    values['price'] = 0.00000800
+    values['num'] = 1000
+    values['type'] = 'TCO'
+    values['danwei'] = 'ETH'
+    values['csrf'] = csrf
+    values['trade_psw'] = 'btex20180619'
+    
+    #临时解决https的问题
+    response = session.post(url,values,verify=True)
+    print(response.text)
+
+def trade_tco_eth():
+    response = session.post('https://btex.com/trade/'+trade_pair,[],verify=True)
+    p = re.compile(r'<input type=\"hidden\" id=\"csrf\" value=\"(.+?)\" />')
+    m = p.search(response.text)
+    print(m.group())
+    csrf = m.group()[38:70]
+
+    url = "https://btex.com/priapi1/sell_coin"
+    values = {}
+    values['price'] = 0.00000800
+    values['num'] = 1000
+    values['type'] = 'TCO'
+    values['danwei'] = 'ETH'
+    values['csrf'] = csrf
+    values['trade_psw'] = 'btex20180619'
     
     #临时解决https的问题
     response = session.post(url,values,verify=True)
@@ -84,7 +127,17 @@ if __name__ == '__main__':
     # trade()
     # post_price()
     # post_trade()
-    order_book()
+
+
+
+    login()
+    buy_price,sell_price = order_book()
+    while buy_price < sell_price:
+        trade_eth_tco()
+        trade_tco_eth()
+        buy_price,sell_price = order_book()
+
+
     # trade_history()
     # buy_coin()
     # while(True):
